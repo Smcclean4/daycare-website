569ad9d65cb0e202761401bd60ef94cf4ed54ed6
Smcclean4 <lsaudim5@outlook.com>
2025-12-15 22:40:31 -0800

    fixing image issues

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -21,7 +21,7 @@ export default function Gallery() {
   const images = [
     ["IMG_1377.jpg", "IMG_1378.jpg", "IMG_1379.jpg", "IMG_6839.jpg", "family_image_7.png"],
     ["IMG_6904.jpeg", "IMG_6838.jpg", "IMG_6889.png", "IMG_6879.PNG", "IMG_6878.PNG", "IMG_6876.PNG", "IMG_6877.PNG", "IMG_6881.PNG", "IMG_6865.jpg"],
-    ["IMG_6884.PNG", "IMG_6885.PNG", "IMG_6886.PNG", "IMG_6887.PNG", "IMG_6888.PNG", "IMG_6893.PNG", "IMG_6880.JPG", "IMG_8257.JPG", "IMG_4244.jpeg", "IMG_1034.jpeg", "IMG_2164.jpeg", "IMG_4535.png", "IMG_4544.png", "IMG_4549.png", "IMG_4562.png", "IMG_5325.png", "IMG_5412.png", "IMG_5493.png", "IMG_5503.png", "IMG_5528.png"],
+    ["IMG_6884.PNG", "IMG_6885.PNG", "IMG_6886.PNG", "IMG_6887.PNG", "IMG_6888.PNG", "IMG_6893.PNG", "IMG_6880.JPG", "IMG_8257.JPG", "IMG_4244.jpeg", "IMG_1034.jpeg", "IMG_2164.jpeg", "IMG_4535.png", "IMG_4544.jpg", "IMG_4549.jpg", "IMG_4562.jpg", "IMG_5325.jpg", "IMG_5412.jpg", "IMG_5493.jpeg", "IMG_5503.jpeg", "IMG_5528.jpeg"],
     ["IMG_6083.jpg", "IMG_0789.jpeg", "IMG_4915.png"]
   ];
 
